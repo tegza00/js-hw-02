1e3efda7b863d0ecb0cba58dbd04819f29e13808
tegza00 <139542222+tegza00@users.noreply.github.com>
2023-07-19 15:50:51 -0700

    homework

---
+++ hw.js/task-1.js
@@ -0,0 +1,5 @@
+const name = "Гренератор"
+let price = 1000
+console.log(name + " " + price);
+price = 2000
+console.log(`'Обрано «Генератор», ціна за одиницю ${price} кредитів.`);
---
+++ hw.js/task-2.js
@@ -0,0 +1,6 @@
+const total = 100; 
+const ordered = 50;
+if(ordered <= total){
+console.log('Замовлення оформлено');
+} else if(ordered > total) 
+    console.log('На складі недостатня кількість товару!');
